26fc5166db37d5f1b69b1cfe46848e4e2c0a13a1
Shaheenhowa05 <shaheenhowa05@gmail.com>
2025-02-13 16:39:47 -0500

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,21 @@
 // Import libraries here
-
+import java.util.Random;
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
-  }
-}
+    // Add your code here }
+
+   System.out.println("SSSSS   H     H");
+   System.out.println("S       H     H");
+   System.out.println("S       H     H");
+   System.out.println(" SSSS   HHHHHHH");
+   System.out.println("     S  H     H");
+   System.out.println("     S  H     H");
+   System.out.println("SSSSS   H     H");
+
+
+  Random generator = new Random();
+  int num1;
+
+  num1 = generator.nextInt(13352) + 32;
+  System.out.println("Your Random Number:" + num1); }}
+
